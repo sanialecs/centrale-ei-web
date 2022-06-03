74c8e1c9cc3ec656830e65be86cf2732de73ca13
Sani Alexandre <alexandre.sani@student-cs.fr>
2022-06-03 16:54:23 +0200

    tp1 encore les styles avec flex

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -7,28 +7,24 @@
       check out the
       <a href="https://cli.vuejs.org" target="_blank">vue-cli documentation</a>.
     </p>
+    <div v-for="movie in movies">
+      {{ movie.title }}, {{ movie.release_date }}
+    </div>
+    <h2> Essai Component </h2>
+    <Movie v-for="movie in movies" :movie="movie" />
+    <h2>Input</h2>
+    <input type="text" v-model="movieName" placeholder="nom du film" />
+    <span>Film: {{ movieName }}</span>
     <h3>Installed CLI Plugins</h3>
     <ul>
       <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          >babel</a
-        >
+        <a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank">babel</a>
       </li>
       <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router"
-          target="_blank"
-          >router</a
-        >
+        <a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank">router</a>
       </li>
       <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          >eslint</a
-        >
+        <a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank">eslint</a>
       </li>
     </ul>
     <h3>Essential Links</h3>
@@ -58,27 +54,56 @@
         <a href="https://vuex.vuejs.org" target="_blank">vuex</a>
       </li>
       <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          >vue-devtools</a
-        >
+        <a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank">vue-devtools</a>
       </li>
       <li>
         <a href="https://vue-loader.vuejs.org" target="_blank">vue-loader</a>
       </li>
       <li>
-        <a href="https://github.com/vuejs/awesome-vue" target="_blank"
-          >awesome-vue</a
-        >
+        <a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a>
       </li>
     </ul>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+import Movie from '@/components/Movie.vue';
+import Movie1 from '@/components/Movie.vue';
+import Movie2 from '@/components/Movie.vue';
+
 export default {
   name: "Home",
+  components: {
+    Movie
+  },
+  data: function () {
+    return {
+      movieName: ""
+    };
+  },
+  data: function () {
+    return {
+      movies: []
+    };
+  },
+  methods: {
+    fetchMovies: function () {
+      axios
+        .get("https://api.themoviedb.org/3/movie/popular?api_key=57359ff087905e870d40ba4880a1dce0")
+        .then((response) => {
+          this.movies = response.data.results;
+          console.log(response.data.results);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  },
+  created() {
+    this.fetchMovies();
+  },
+  components: { Movie, Movie1, Movie2 }
 };
 </script>
 
