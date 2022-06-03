a3516b812303fdb2179dcc28b695ea5b5232c0df
Sani Alexandre <alexandre.sani@student-cs.fr>
2022-06-03 16:54:44 +0200

    tp1 encore les styles avec flex

---
+++ frontend/src/components/Movie.vue
@@ -0,0 +1,17 @@
+<template>
+    <ul>
+        {{ movie.title }}, {{ movie.release_date }}
+        <img :src="'https://www.themoviedb.org/t/p/original' + movie.poster_path" />
+    </ul>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    name: "Movie",
+    props: {
+        movie: Array
+    }
+}
+</script>
